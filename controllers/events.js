9a167608f7119a5cb185caf6026337766cf23f3d
Claude Allen <claudeallen@Claudes-MacBook-Pro.local>
2023-01-02 12:10:00 -0600

    delete route created

--- controllers/events.js
+++ controllers/events.js
@@ -5,17 +5,11 @@ const Event = require("../models/eventModel");
 
 //induces
 //i ////EVENT index page, all the other parts of this file should be hidden for users thatt do not have tthe login i create at thhe end
-
 router.get("/gasandnectar", (req, res) => {
   Event.find(req.params.id, (err, event) => {
     res.render("events/index.ejs", { event });
   });
 });
-
-
-
-
-///////////////// leaving it naked until i figure out how to integrate more files  
 
 //n
 router.get('/gasandnectar/new', (req, res) => {
@@ -24,12 +18,10 @@ router.get('/gasandnectar/new', (req, res) => {
   })
 })
 
-
-
 //d 
-router.delete("/:id", (req, res) => {
+router.delete("/gasandnectar/:id", (req, res) => { //labeling gas adn nectar in the route in order to target where the deletion is happening 
   Event.findByIdAndRemove(req.params.id, () => {
-    res.redirect("events/gasandnectar");
+    res.redirect("/events/gasandnectar");
   });
 });
 
@@ -39,7 +31,6 @@ router.put("/gasandnectar/:id", (req, res) => {
     res.redirect("/events/gasandnectar"); // update route needs to match the landing of whwere the data needs to go
   });
 });
-
 
 //c
 router.post("/gasandnectar", (req, res) => { //TODO 1.1.23 fix edit delete and new routes. edit needs a destination, delete isnt working, and new hasnt been created 
@@ -56,7 +47,6 @@ router.get("/gasandnectar/:id/edit",(req, res) => {
     });
   }
 );
-
 
 //s
 router.get("/gasandnectar/:id",(req, res) => {
