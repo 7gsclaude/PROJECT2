d2fe19bbd5697b07d8b9e61840345488f36cb4f2
Claude Allen <claudeallen@Claudes-MacBook-Pro.local>
2023-01-02 11:58:24 -0600

    fixed edit route completely add and delete are left

--- controllers/events.js
+++ controllers/events.js
@@ -7,7 +7,6 @@ const Event = require("../models/eventModel");
 //i ////EVENT index page, all the other parts of this file should be hidden for users thatt do not have tthe login i create at thhe end
 
 router.get("/gasandnectar", (req, res) => {
-  ///todo write out models
   Event.find(req.params.id, (err, event) => {
     res.render("events/index.ejs", { event });
   });
@@ -30,29 +29,30 @@ router.get('/gasandnectar/new', (req, res) => {
 //d 
 router.delete("/:id", (req, res) => {
   Event.findByIdAndRemove(req.params.id, () => {
-    res.redirect("event/gasandnectar");
+    res.redirect("events/gasandnectar");
   });
 });
 
 //u
-router.put("/:id", (req, res) => {
+router.put("/gasandnectar/:id", (req, res) => {
   Event.findByIdAndUpdate(req.params.id, req.body, () => {
-    res.redirect("event/gasandnectar");
+    res.redirect("/events/gasandnectar"); // update route needs to match the landing of whwere the data needs to go
   });
 });
 
 
 //c
-router.post("/gasandnectar/", (req, res) => { //TODO 1.1.23 fix edit delete and new routes. edit needs a destination, delete isnt working, and new hasnt been created 
+router.post("/gasandnectar", (req, res) => { //TODO 1.1.23 fix edit delete and new routes. edit needs a destination, delete isnt working, and new hasnt been created 
   Event.create(req.body, (err, event) => {
-    res.redirect("event/gasandnectar/");
+    res.redirect("events/gasandnectar/");
   });
 });
 
 //e
 router.get("/gasandnectar/:id/edit",(req, res) => {
     Event.findById(req.params.id, (err, event) => {
-      res.render("events/edit.ejs", { event });
+        res.render("events/edit.ejs", { event });
+        console.log(event);
     });
   }
 );
@@ -61,7 +61,6 @@ router.get("/gasandnectar/:id/edit",(req, res) => {
 //s
 router.get("/gasandnectar/:id",(req, res) => {
     Event.findById(req.params.id, (err, event) => {
-      console.log(event); //will populate author into object
       res.render(
         "events/show.ejs",
         { event }
